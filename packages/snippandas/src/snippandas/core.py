823c876c55878937b8ca33383b0215c9f11b9619
oyghen <oyghen@duck.com>
2025-11-30 16:29:06 +0100

    feat(snippandas): add union

--- packages/snippandas/src/snippandas/core.py
+++ packages/snippandas/src/snippandas/core.py
@@ -1,6 +1,7 @@
-__all__ = ("display", "profile")
+__all__ = ("display", "profile", "union")
 
 import functools
+from collections.abc import Iterable
 
 import numpy as np
 import pandas as pd
@@ -219,3 +220,76 @@ def get_shell_type() -> str:
 
     except (ModuleNotFoundError, ImportError, NameError):
         return "python"
+
+
+def union(dataframes: Iterable[PandasDF]) -> PandasDF:
+    """Union iterable of Pandas dataframes by name with index reset.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from snippandas import core
+    >>> df1 = pd.DataFrame([dict(x=1, y=2), dict(x=3, y=4)])
+    >>> df2 = pd.DataFrame([dict(x=5, y=6), dict(x=7, y=8)])
+    >>> df3 = pd.DataFrame([dict(x=0, y=1), dict(x=2, y=3)])
+    >>> core.union([df1, df2, df3])
+       x  y
+    0  1  2
+    1  3  4
+    2  5  6
+    3  7  8
+    4  0  1
+    5  2  3
+
+    >>> df1 = pd.DataFrame([[1, 2], [3, 4]], index=[0, 1])
+    >>> df2 = pd.DataFrame([[5, 6], [7, 8]], index=[0, 2])
+    >>> core.union([df1, df2])
+       0  1
+    0  1  2
+    1  3  4
+    2  5  6
+    3  7  8
+
+    >>> df1 = pd.DataFrame([[1, 2], [3, 4]], index=[0, 1], columns=["a", "b"])
+    >>> df2 = pd.DataFrame([[5, 6], [7, 8]], index=[0, 2], columns=["c", "d"])
+    >>> core.union([df1, df2])
+         a    b    c    d
+    0  1.0  2.0  NaN  NaN
+    1  3.0  4.0  NaN  NaN
+    2  NaN  NaN  5.0  6.0
+    3  NaN  NaN  7.0  8.0
+
+    >>> df1 = pd.DataFrame([[1, 2], [3, 4]])
+    >>> s1 = pd.Series([5, 6])
+    >>> core.union([df1, s1])
+       0    1
+    0  1  2.0
+    1  3  4.0
+    2  5  NaN
+    3  6  NaN
+
+    >>> s1 = pd.Series([1, 2])
+    >>> s2 = pd.Series([3, 4])
+    >>> s3 = pd.Series([5, 6])
+    >>> core.union([s1, s2, s3])
+       0
+    0  1
+    1  2
+    2  3
+    3  4
+    4  5
+    5  6
+
+    >>> s1 = pd.Series([1, 2], index=[0, 1], name="a")
+    >>> s2 = pd.Series([3, 4], index=[1, 2], name="b")
+    >>> s3 = pd.Series([5, 6], index=[2, 3], name="c")
+    >>> core.union([s1, s2, s3])
+         a    b    c
+    0  1.0  NaN  NaN
+    1  2.0  NaN  NaN
+    2  NaN  3.0  NaN
+    3  NaN  4.0  NaN
+    4  NaN  NaN  5.0
+    5  NaN  NaN  6.0
+    """
+    return pd.concat(map(pd.DataFrame, dataframes), axis=0, ignore_index=True)
--- packages/snippandas/tests/test_snippandas_core.py
+++ packages/snippandas/tests/test_snippandas_core.py
@@ -102,3 +102,21 @@ def test_join():
 
     expected = pd.merge(df1, df2, on="a")
     pd.testing.assert_frame_equal(result, expected)
+
+
+def test_union():
+    cols = ("a", "b")
+
+    idx1 = (0, 1)
+    row11, row12 = (1, 2), (3, 4)
+    df1 = pd.DataFrame([row11, row12], idx1, cols)
+
+    idx2 = (2, 3)
+    row21, row22 = (5, 6), (7, 8)
+    df2 = pd.DataFrame([row21, row22], idx2, cols)
+
+    actual = core.union([df1, df2])
+    assert isinstance(actual, pd.DataFrame)
+
+    expected = pd.DataFrame([row11, row12, row21, row22], idx1 + idx2, cols)
+    pd.testing.assert_frame_equal(actual, expected)
